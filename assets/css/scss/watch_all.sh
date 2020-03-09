7973650a6f686b196c6a8f29293c8d401d33b6af
fc-anjos <felipe.anjos@usp.br>
2020-03-09 18:51:37 -0300

    use scss extension for stylelint

--- assets/css/scss/watch_all.sh
+++ assets/css/scss/watch_all.sh
@@ -1,4 +1,5 @@
+
 #/bin/zsh
 
 sass --watch ./chat.scss: ../chat.css;
-sass--watch./chat.scss ./person_detail.scss: ../person_detail.css;./person_detail.scss
+sass--watch./chat.scss ./person_detail.scss: ../person_detail.css;./person_detail.scss./person_detail.scss
