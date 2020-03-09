44ae6a144a52b4b9031f8b46b6833091783bdcb5
fc-anjos <felipe.anjos@usp.br>
2020-03-09 13:44:03 -0300

    .gitignore fix

--- assets/css/scss/watch_all.sh
+++ assets/css/scss/watch_all.sh
@@ -1,3 +1,5 @@
+
 #/bin/zsh
 
-sass --watch ./chat.scss:../chat.css ./person_detail.scss:../person_detail.css
+sass --watch ./chat.scss: ../chat.css;
+sass--watch./chat.scss ./person_detail.scss: ../person_detail.css;./person_detail.scss
