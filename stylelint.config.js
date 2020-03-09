dcb0a579ac44391b7729b2c73dbc63db9b4d2c57
fc-anjos <felipe.anjos@usp.br>
2020-03-09 19:03:11 -0300

    track package-lock.json

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,7 @@
 "use strict"
 
 module.exports = {
-  "extends": "stylelint-config-recommended",
+  "extends": ["stylelint-config-recommended", "stylelint-config-sass-guidelines"],
   "rules": {
     "at-rule-empty-line-before": [ "always", {
       except: [
